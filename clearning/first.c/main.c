e7090062894b504cb75e3888cd51947c4abc0680
zahidkhan262 <zahidkhan007860@gmail.com>
2021-12-08 12:08:22 +0530

    add 5 to 1 patter

--- clearning/first.c/main.c
+++ clearning/first.c/main.c
@@ -6,7 +6,13 @@ int main()
     int i,j, n=5;
     for(i=0; i<=n; i++){
         for(j=1; j<=i; j++){
-            printf("%d","i"+j-1);
+            printf("%c","A"+j-1);
+        }
+        printf("\n");
+    }
+      for(i=n; i>=1; i--){
+        for(j=1; j<=i; j++){
+            printf("%c","A"-1+i);
         }
         printf("\n");
     }
